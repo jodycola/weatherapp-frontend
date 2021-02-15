5af0745f7694b7e0b59202449c03c2eb9317029c
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-02-15 18:28:44 -0500

    added weather cards to favorites div

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 //url section
-const railsData = 'http://localhost:3000/users'
+const usersData = 'http://localhost:3000/users'
+const favoritesData = 'http://localhost:3000/favorites'
 const weatherUrl = ''
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip='
 
 //document elements
 const form = document.querySelector("#location-form")
-const favoriteDiv = document.querySelector('#favorites')
+const favoriteDiv = document.querySelector('div.weather-wrapper')
 const homeWeather = document.querySelector('.detail-image')
 const homeLocation = document.querySelector('.title')
+const userID = 1
 
 //fetch functions
 const getUser = _ => {
-    fetch(railsData + '/1')
+    fetch(usersData + '/1')
         .then(res => res.json())
         .then(dataArr => {
             dataArr.favorites.forEach(favorite => {
-                getWeatherForLocation(favorite.location.zip)
+                getWeatherForLocation(favorite.zip)
             })
         })
+        form.dataset.id = userID
 }
 
 getUser()
 
 const getLocation = event => {
     event.preventDefault()
-    const userId = event.target.dataset.id
+    const user_id = event.target.dataset.id
     const zip = event.target.location.value
+
+    const newFavorite = {user_id, zip}
+
+    configObj = {
+        method: "POST",
+        headers: { "Content-type":"application/json" },
+        body: JSON.stringify(newFavorite)
+    }
+
+    fetch(favoritesData, configObj)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+
 
     // createHomeLocationForUser(zip)
     getWeatherForLocation(zip)
@@ -41,13 +59,20 @@ const getWeatherForLocation = zip => {
 //display functions
 
 const createNewFavorite = weather => {
-    const span = document.createElement('span')
-    const h5 = document.createElement('h5')
-    h5.innerText = weather.name
-    span.innerText = weather.main.temp
-    // const favBtn
-    span.append(h5)
-    favoriteDiv.append(span)
+    const favoriteCard = document.createElement('div')
+    favoriteCard.className = "weather-card"
+
+    const favoriteIcon = document.createElement('div')
+    favoriteIcon.className = "weather-icon sun"
+    
+    const h1 = document.createElement('h1')
+    h1.innerText = Math.round(weather.main.temp) + '°'
+
+    const p = document.createElement('p')
+    p.innerText = weather.name
+
+    favoriteCard.append(p, h1, favoriteIcon)
+    favoriteDiv.append(favoriteCard)
 }
 
 
